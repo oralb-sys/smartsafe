69efc1492bff171f4e65743b7a51422454e6425a
Rony Villafuerte <243105@unsaac.edu.pe>
2026-09-09 23:39:07 -0500

    feat(backend): crear API base de SmartSafe

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+﻿const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        mensaje: 'API SmartSafe funcionando'
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Servidor SmartSafe ejecutándose en http://localhost:' + PORT);
+});
